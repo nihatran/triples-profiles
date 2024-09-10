740b1a62e169e1725181736ebd3c28ffd7f79248
nihatran <89045243+nihatran@users.noreply.github.com>
2024-09-09 23:44:34 -0400

    Add card flip

---
+++ triples-profiles/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Card } from "./Card";
+
+export default function App() {
+  return (
+    <>
+      <div className="header">
+        <h1>tripleS</h1>
+      </div>
+      <Card />
+    </>
+  );
+}
---
+++ triples-profiles/src/Card.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./Card.css";
+
+export function Card() {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  function handleClick() {
+    setIsFlipped(!isFlipped);
+  }
+
+  return (
+    <>
+      <div class="container">
+        <div
+          className={`card ${isFlipped ? "isFlipped" : null}`}
+          onClick={handleClick}
+        >
+          <div className="cardFront">Front</div>
+          <div className="cardBack">Back</div>
+        </div>
+      </div>
+    </>
+  );
+}
