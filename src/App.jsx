d8cab9a2212506afe4afcf83fd5c261cb2046503
nihatran <89045243+nihatran@users.noreply.github.com>
2024-09-14 17:27:04 -0400

    Add member data

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,18 @@
 import "./App.css";
 import { Card } from "./Card";
+import { members } from "./membersData";
 
 export default function App() {
+  let memberCards = members.map((member) => {
+    return <Card key={member.id} member={member} />;
+  });
+
   return (
     <>
       <div className="header">
         <h1>tripleS</h1>
       </div>
-      <div className="cardsContainer">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      <div className="cardsContainer">{memberCards}</div>
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import "./Card.css";
 
-export function Card() {
+export function Card({ member }) {
   const [isFlipped, setIsFlipped] = useState(false);
 
   function handleClick() {
@@ -15,8 +15,25 @@ export function Card() {
           className={`card ${isFlipped ? "isFlipped" : null}`}
           onClick={handleClick}
         >
-          <div className="cardFront">Front</div>
-          <div className="cardBack">Back</div>
+          <div className="cardFront">
+            <img src={member.picture} alt="member photo"></img>
+          </div>
+          <div className="cardBack">
+            <p>{member.number}</p>
+            <p>Name: {member.name}</p>
+            <p>Birthday: {member.bday}</p>
+            <div className="colorRow">
+              <p>Color: &nbsp;</p>
+              <div
+                style={{
+                  backgroundColor: `${member.color}`,
+                  height: "15px",
+                  width: "15px",
+                }}
+              ></div>
+            </div>
+            <p>Emoji: {member.emoji}</p>
+          </div>
         </div>
       </div>
     </>
